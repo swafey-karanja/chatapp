7f52f2eb7b450cbebf233e5b7db9abf60b620b0b
swafey karanja <kswafey@gmail.com>
2024-03-06 23:45:03 +0300

    Chatbot UI

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import Cam from "../img/cam.png"
-import More from "../img/more.png"
-import Add from "../img/add.png"
 import Messages from './Messages'
 import Input from './Input'
 
 const Chat = () => {
   return (
     <div className='chat'>
-      <div className="chatInfo">
-        <span>Itachi</span>
-        <div className="chatIcons">
-          <img src={Cam} alt="" />
-          <img src={Add} alt="" />
-          <img src={More} alt="" />
-        </div>
-      </div>
       <Messages/>
       <Input/>
     </div>
